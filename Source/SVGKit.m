39f3f91e93f65c65a299855fa34fd97069f5508f
BubbleTrouble14 <ronald.goedeke@outlook.com>
2025-11-23 09:24:32 -0300

    Fix CocoaLumberjack import for compatibility with newer versions

--- Source/SVGKit.m
+++ Source/SVGKit.m
@@ -7,12 +7,23 @@
 //
 
 #import "SVGKit.h"
-#import "CocoaLumberjack/DDOSLogger.h"
+
+#if __has_include(<CocoaLumberjack/CocoaLumberjack.h>)
+#import <CocoaLumberjack/CocoaLumberjack.h>
+#elif __has_include("CocoaLumberjack/CocoaLumberjack.h")
+#import "CocoaLumberjack/CocoaLumberjack.h"
+#endif
 
 @implementation SVGKit : NSObject
 
 + (void) enableLogging {
-    [DDLog addLogger:[DDOSLogger sharedInstance]];
+#if __has_include(<CocoaLumberjack/CocoaLumberjack.h>) || __has_include("CocoaLumberjack/CocoaLumberjack.h")
+    #if TARGET_OS_IOS || TARGET_OS_TV
+        [DDLog addLogger:[DDOSLogger sharedInstance]];
+    #else
+        [DDLog addLogger:[DDASLLogger sharedInstance]];
+    #endif
+#endif
 }
 
 @end
